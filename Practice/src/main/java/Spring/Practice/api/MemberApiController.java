70aba11361f903edc944a56a362f3e2eb0bdefbb
장우진 <dnwls16071@naver.com>
2024-07-24 00:24:41 +0900

    :recycle: 회원 조회 V2 REST API 개발

--- Practice/src/main/java/Spring/Practice/api/MemberApiController.java
+++ Practice/src/main/java/Spring/Practice/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class MemberApiController {
@@ -45,6 +46,37 @@ public class MemberApiController {
 	@GetMapping("/api/v1/members")
 	public List<Member> membersV1() {
 		return memberService.findMembers();
+	}
+
+	// Result 제네릭 클래스로 컬렉션을 감싸면 향후 필요한 필드 추가도 쉽게 할 수 있다.
+	// 컬렉션을 직접 반환하게 되면 향후 API 스펙을 변경하기가 매우 까다로워진다.
+	// 별도의 Result 클래스 생성으로 해결이 가능하다.ㄷ
+	@GetMapping("/api/v2/members")
+	public Result membersV2() {
+		List<Member> members = memberService.findMembers();
+		List<MemberDTO> collect = members.stream()
+				.map(member -> new MemberDTO(member.getName()))
+				.collect(Collectors.toList());
+		return new Result(collect);
+	}
+
+	// 제네릭 도입
+	@Data
+	static class Result<T> {
+		private T data;
+
+		public Result(T data) {
+			this.data = data;
+		}
+	}
+
+	@Data
+	static class MemberDTO {
+		private String name;
+
+		public MemberDTO(String name) {
+			this.name = name;
+		}
 	}
 
 	@Data
