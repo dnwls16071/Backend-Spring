59c945dd3c5b9e78ab83733d3745544ab871bab2
장우진 <dnwls16071@naver.com>
2024-07-23 23:44:11 +0900

    :sparkles: 회원 등록 V1 REST API 개발

---
+++ Practice/src/main/java/Spring/Practice/api/MemberApiController.java
@@ -0,0 +1,35 @@
+package Spring.Practice.api;
+
+import Spring.Practice.domain.Member;
+import Spring.Practice.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberApiController {
+
+	private final MemberService memberService;
+	@Autowired
+	public MemberApiController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	@PostMapping("/api/v1/members")
+	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+
+	@Data
+	static class CreateMemberResponse {
+		private Long id;
+
+		public CreateMemberResponse(Long id) {
+			this.id = id;
+		}
+	}
+}
