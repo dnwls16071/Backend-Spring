15f446cc50d53af21f7b105d7193161f9c47f927
장우진 <dnwls16071@naver.com>
2024-07-24 00:05:06 +0900

    :sparkles: 회원 수정 V2 REST API 개발

--- Practice/src/main/java/Spring/Practice/api/MemberApiController.java
+++ Practice/src/main/java/Spring/Practice/api/MemberApiController.java
@@ -6,9 +6,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MemberApiController {
@@ -34,6 +32,15 @@ public class MemberApiController {
 		return new CreateMemberResponse(id);
 	}
 
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(@PathVariable(name = "id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
+		// 변경의 경우 가급적 반환값이 없게끔 설계하는 것이 좋다.
+		memberService.update(id, request.getName());
+		Member member = memberService.findOne(id);
+		return new UpdateMemberResponse(member.getId(), member.getName());
+	}
+
+
 	@Data
 	static class CreateMemberResponse {
 		private Long id;
@@ -48,6 +55,22 @@ public class MemberApiController {
 		private Long id;
 
 		@NotEmpty
+		private String name;
+	}
+
+	@Data
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
+
+		public UpdateMemberResponse(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+	}
+
+	@Data
+	static class UpdateMemberRequest {
 		private String name;
 	}
 }
