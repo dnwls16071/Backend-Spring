dce3d0093e6ee849380401095ffa0ffa6f685f61
장우진 <dnwls16071@naver.com>
2024-07-24 13:56:26 +0900

    :recycle: 주문 조회 ordersV5(JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화) REST API 개발

--- Practice/src/main/java/Spring/Practice/repository/order/query/OrderQueryRepository.java
+++ Practice/src/main/java/Spring/Practice/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 // 엔티티가 아닌 다른 것들을 조회하기 위한 별도의 리포지토리
 @Repository
@@ -34,7 +36,29 @@ public class OrderQueryRepository {
 				.setParameter("orderId", orderId)
 				.getResultList();
 	}
-	
+
+	public List<OrderQueryDto> findAllByDto_optimization() {
+		List<OrderQueryDto> result = findOrders();
+
+		// findOrders() → order의 식별자 orderId
+		// orderId 리스트
+		List<Long> orderIds = result.stream()
+				.map(o -> o.getOrderId())
+				.collect(Collectors.toList());
+
+		// 파라미터에 orderId리스트를 넣고 IN절 사용
+		List<OrderItemQueryDto> orderItems = em.createQuery("select new Spring.Practice.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count) from OrderItem oi join oi.item i where oi.order.id in :orderIds", OrderItemQueryDto.class)
+				.setParameter("orderIds", orderIds)
+				.getResultList();
+
+		// OrderItemQueryDto → (Long orderId, String itemName, int orderPrice, int count)
+		// 식별자 orderId를 가져와서 메모리 상에서 그룹화
+		// 그렇게 되면 하나의 orderId에 있는 OrderItem 컬렉션이 담기게 되고 orderId가 key, 컬렉션이 value가 됨
+		Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream().collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+		result.forEach(o -> o.setOrderItemQueryDtoList(orderItemMap.get(o.getOrderId())));
+		return result;
+	}
+
 	// [1]. ToOne 관계들을 먼저 처리
 	// ORDER : MEMBER = N : 1
 	// ORDER : DELIVERY = 1 : 1
