1f5261560d9522f021addbfb70af4336a83a7b19
장우진 <dnwls16071@naver.com>
2024-07-25 10:42:06 +0900

    :white_check_mark: main 함수 실행하여 결과 검증(주문 검증 테스트)

--- ex-hello-jpa/src/main/java/jpabasic/JpaRunner.java
+++ ex-hello-jpa/src/main/java/jpabasic/JpaRunner.java
@@ -4,8 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jpabasic.domain.Member;
-import jpabasic.domain.Team;
+import jpabasic.domain.*;
 
 import java.util.List;
 
@@ -18,27 +17,24 @@ public class JpaRunner {
 		tx.begin();
 
 		try {
-			Team team = new Team();
-			team.setName("team");
-			em.persist(team);
-
-			Team newTeam = new Team();
-			newTeam.setName("이전할 팀");
-			em.persist(newTeam);
+			Item item = new Item();
+			item.setName("잡동사니");
+			em.persist(item);
 
 			Member member = new Member();
-			member.setName("member");
-			member.setTeam(team);
+			member.setName("고객");
 			em.persist(member);
 
-			em.flush();	// 영속성 컨텍스트 플러시 → 쓰기 지연 SQL 저장소에 등록된 쿼리를 DB에 반영
-			em.clear();	// 영속성 컨텍스트 초기화
+			// 잡동사니 상품 주문 추가
+			OrderItem orderItem = new OrderItem();
+			orderItem.setItem(item);
+			em.persist(orderItem);
 
-			Member findMember = em.find(Member.class, member.getId());// 1차 캐시에 없기에 우선적으로 DB를 조회하여 1차 캐시로 가져오고 영속성 컨텍스트에서 관리되도록
-			findMember.setTeam(newTeam);	// 새로운 팀으로 변경(수정)
-
-			findMember.setTeam(null);	// 엔티티 삭제(연관관계를 제거해줘야 에러가 발생하지 않는다.)
-			em.persist(findMember);
+			// 주문 생성
+			Order order = new Order();
+			order.setMember(member);
+			order.addOrderItem(orderItem);	// 연관관계 편의 메서드 사용
+			em.persist(order);
 
 			tx.commit();
 		} catch (Exception e) {
