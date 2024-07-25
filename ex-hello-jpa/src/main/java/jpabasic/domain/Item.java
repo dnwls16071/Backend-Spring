3985f857e815496178bbf1224bbb76ab376984a8
장우진 <dnwls16071@naver.com>
2024-07-25 11:14:21 +0900

    :sparkles: Category 엔티티 추가에 따른 요구사항 변화 반영

--- ex-hello-jpa/src/main/java/jpabasic/domain/Item.java
+++ ex-hello-jpa/src/main/java/jpabasic/domain/Item.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -16,4 +19,7 @@ public class Item {
 	private String name;
 	private int price;
 	private int stockQuantity;
+
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categories = new ArrayList<>();
 }
