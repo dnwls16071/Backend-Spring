634eb0c70916fd054264c42d25c53602ca097a62
장우진 <dnwls16071@naver.com>
2024-07-25 12:09:00 +0900

    :sparkles: Order-Delivery & Order-OrderItem 영속성 전이 추가

--- ex-hello-jpa/src/main/java/jpabasic/domain/Order.java
+++ ex-hello-jpa/src/main/java/jpabasic/domain/Order.java
@@ -26,10 +26,12 @@ public class Order extends BaseEntity {
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	// 주문 엔티티를 관리함에 따라 주문상품 엔티티 역시 같이 관리됨
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne(fetch = FetchType.LAZY)
+	// 주문 엔티티를 관리함에 따라 배송 엔티티 역시 같이 관리됨
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
 	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 
