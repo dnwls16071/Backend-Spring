bd930508c066d88a1ee34a6a296c143e08345686
장우진 <dnwls16071@naver.com>
2024-07-25 10:40:45 +0900

    :sparkles: 새로운 엔티티 추가 Item

---
+++ ex-hello-jpa/src/main/java/jpabasic/domain/Item.java
@@ -0,0 +1,19 @@
+package jpabasic.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "ITEM")
+public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "item_id")
+	private Long id;
+	private String name;
+	private int price;
+	private int stockQuantity;
+}
