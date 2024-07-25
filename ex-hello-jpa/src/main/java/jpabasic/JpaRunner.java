a010485e4d88c76e94b94a59bc2249a5d14c0c54
장우진 <dnwls16071@naver.com>
2024-07-25 10:12:00 +0900

    :white_check_mark: main 함수 실행하여 결과 검증(객체 그래프 탐색 및 JPQL 쿼리문 작성하여 테스트하기)

--- ex-hello-jpa/src/main/java/jpabasic/JpaRunner.java
+++ ex-hello-jpa/src/main/java/jpabasic/JpaRunner.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Persistence;
 import jpabasic.domain.Member;
 import jpabasic.domain.Team;
 
+import java.util.List;
+
 public class JpaRunner {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -24,6 +26,15 @@ public class JpaRunner {
 			member.setName("member");
 			member.setTeam(team);
 			em.persist(member);
+
+			Team findTeam = member.getTeam();
+			System.out.println("팀 이름: " + findTeam.getName());	// 객체 그래프 탐색
+
+			// team에 속한 모든 팀원을 조회하는 JPQL 쿼리문 작성해보기
+			List<Member> resultList = em.createQuery("select m from Member as m join m.team as t where t.name = :teamName", Member.class)
+					.setParameter("teamName", "team")
+					.getResultList();
+			resultList.forEach(m -> System.out.println(m.getName()));
 
 			tx.commit();
 		} catch (Exception e) {
