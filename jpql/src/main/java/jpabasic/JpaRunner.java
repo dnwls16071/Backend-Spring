7a444ce23ccad6edb79581e00fc253544dd75b59
장우진 <dnwls16071@naver.com>
2024-07-26 10:45:05 +0900

    :white_check_mark: main 함수 실행하여 결과 확인(단일 값 경로 탐색)

--- jpql/src/main/java/jpabasic/JpaRunner.java
+++ jpql/src/main/java/jpabasic/JpaRunner.java
@@ -15,100 +15,41 @@ public class JpaRunner {
 		tx.begin();
 
 		try {
-			Team team1 = new Team();
-			team1.setName("팀A");
-			em.persist(team1);
-
-			Team team2 = new Team();
-			team2.setName("팀B");
-			em.persist(team2);
+			// 주어진 케이스
+			Team team = new Team();
+			team.setName("team");
+			em.persist(team);
 
 			Member m1 = new Member();
-			Member m2 = new Member();
-			Member m3 = new Member();
-			Member m4 = new Member();
-			Member m5 = new Member();
-
-			m1.setTeam(team1);
-			m2.setTeam(team1);
-			m3.setTeam(team2);
-			m4.setTeam(team2);
-
+			m1.setUsername("member1");
+			m1.setAge(15);
+			m1.setTeam(team);
 			em.persist(m1);
+
+			Member m2 = new Member();
+			m2.setUsername("member2");
+			m2.setAge(16);
+			m2.setTeam(team);
 			em.persist(m2);
+
+			Member m3 = new Member();
+			m3.setAge(17);
+			m3.setUsername("member3");
+			m3.setTeam(team);
 			em.persist(m3);
-			em.persist(m4);
-			em.persist(m5);
 
-			em.flush();
-			em.clear();
-
-			// 엔티티 내부 조인
-			System.out.println("엔티티 내부 조인");
-			List<Member> join1 = em.createQuery("select m from Member m inner join m.team t", Member.class)
+			// 단일 값 경로 탐색 (1)
+			List<Integer> list = em.createQuery("select m.age from Member m", Integer.class)
 					.getResultList();
-			for (Member member : join1) {
-				System.out.println(member);
+			for (Integer integer : list) {
+				System.out.println(integer);
 			}
 
-			em.flush();
-			em.clear();
-			
-			// 엔티티 외부 조인
-			System.out.println("엔티티 외부 조인");
-			List<Member> join2 = em.createQuery("select m from Member m left join m.team t", Member.class)
+			// 단일 값 경로 탐색 (2)
+			List<String> list1 = em.createQuery("select m.username from Member m", String.class)
 					.getResultList();
-			for (Member member : join2) {
-				System.out.println(member);
-			}
-			
-			em.flush();
-			em.clear();
-			
-			// 컬렉션 조인
-			// 현재 팀A에 소속된 멤버는 총 4명
-			// 일대다 컬렉션 조인의 경우 다(N)에 해당하는 만큼의 데이터 뻥튀기가 발생하는 문제가 발생
-			System.out.println("일대다 컬렉션 조인");
-			List<Team> join3 = em.createQuery("select t from Team t join t.members", Team.class)
-					.getResultList();
-			for (Team t : join3) {
-				System.out.println(t.getName() + " : " + t.getMembers().size());
-			}
-			
-			em.flush();
-			em.clear();
-			
-			// 컬렉션 조인
-			// 현재 팀A에 소속된 멤버는 총 4명
-			// 위에서 제기된 일대다 컬렉션의 문제점을 해결하기 위해 애플리케이션 차원에서 중복을 제거하는 distinct 키워드를 지원
-			System.out.println("일대다 컬렉션 조인 - distinct 키워드");
-			List<Team> join4 = em.createQuery("select distinct t from Team t join t.members", Team.class)
-					.getResultList();
-			for (Team t : join4) {
-				System.out.println(t.getName() + " : " + t.getMembers().size());
-			}
-
-			em.flush();
-			em.clear();
-			
-			// 엔티티 페치 조인
-			// 팀과 멤버를 함께 조인하여 한 번에 가져옴
-			System.out.println("엔티티 페치 조인");
-			List<Member> join5 = em.createQuery("select m from Member m join fetch m.team t", Member.class)
-					.getResultList();
-			for (Member member : join5) {
-				System.out.println(member);
-			}
-
-			// 컬렉션 페치 조인 + distinct
-			System.out.println("컬렉션 페치 조인");
-			List<Team> join6 = em.createQuery("select distinct t from Team t join fetch t.members", Team.class)
-					.getResultList();
-			for (Team t : join6) {
-				System.out.println(t.getName());
-				for (Member member : t.getMembers()) {
-					System.out.println(member);
-				}
+			for (String string : list1) {
+				System.out.println(string);
 			}
 
 			tx.commit();
