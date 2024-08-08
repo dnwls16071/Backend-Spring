dfb2a4fd84280a592cd3cc5644fa1c39267377d0
장우진 <dnwls16071@naver.com>
2024-08-08 20:54:39 +0900

    :sparkles: 엔티티 추가

---
+++ springdatajpa/src/main/java/study/springdatajpa/domain/Member.java
@@ -0,0 +1,26 @@
+package study.springdatajpa.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "MEMBERS")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String username;
+	private int age;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team")
+	private Team team;
+
+	public Member(String username) {
+		this.username = username;
+	}
+}
---
+++ springdatajpa/src/main/java/study/springdatajpa/domain/Team.java
@@ -0,0 +1,27 @@
+package study.springdatajpa.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Team {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@OneToMany(mappedBy = "team")
+	private List<Member> members = new ArrayList<>();
+
+	public void addMember(Member member) {
+		this.getMembers().add(member);
+		member.setTeam(this);
+	}
+}
