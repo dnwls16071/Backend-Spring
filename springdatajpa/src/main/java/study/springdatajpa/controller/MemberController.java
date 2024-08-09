8b124ead012b7bd066320831e1b06bafc7eef0d4
장우진 <dnwls16071@naver.com>
2024-08-09 11:10:40 +0900

    :sparkles: Web 확장 - 페이징과 정렬

--- springdatajpa/src/main/java/study/springdatajpa/controller/MemberController.java
+++ springdatajpa/src/main/java/study/springdatajpa/controller/MemberController.java
@@ -2,11 +2,16 @@ package study.springdatajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import study.springdatajpa.domain.Member;
+import study.springdatajpa.dto.MemberDto;
 import study.springdatajpa.repository.MemberRepository;
+
+import java.util.stream.Collectors;
 
 @RestController
 public class MemberController {
@@ -32,8 +37,21 @@ public class MemberController {
 		return member.getUsername();
 	}
 
+	// 스프링 데이터 JPA에서 제공하는 페이징과 정렬 사용
+	// 특정 페이지 데이터 조회 : /members?page=2
+	// 정렬 : /members?page=2&sort=id,desc
+	// 거듭 강조하지만 엔티티를 직접 외부에 노출시키는 것은 금한다.
+	// 기본 사이즈 : 20
+	@GetMapping("/members")
+	public Page<MemberDto> list(Pageable pageable) {
+		Page<Member> page = memberRepository.findAll(pageable);
+		return page.map(MemberDto::new);
+	}
+
 	@PostConstruct
 	public void init() {
-		memberRepository.save(new Member("member"));
+		for (int i = 0; i < 100; i++) {
+			memberRepository.save(new Member("member" + i, i));
+		}
 	}
 }
--- springdatajpa/src/main/java/study/springdatajpa/dto/MemberDto.java
+++ springdatajpa/src/main/java/study/springdatajpa/dto/MemberDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import study.springdatajpa.domain.Member;
 
 @Getter
 @Setter
@@ -13,4 +14,11 @@ public class MemberDto {
 	private Long id;
 	private String username;
 	private String teamName;
+
+	// 엔티티를 파라미터로 넣어 DTO로 변환
+	public MemberDto(Member member) {
+		this.id = member.getId();
+		this.username = member.getUsername();
+		this.teamName = member.getTeam().getName();
+	}
 }
