54a16486528efe8db6994b74b4439917d5ff065b
장우진 <dnwls16071@naver.com>
2024-08-18 22:52:07 +0900

    :sparkles: 서블릿 예외 처리(필터)

---
+++ exception/src/main/java/hello/exception/config/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.exception.config;
+
+import hello.exception.filter.LogFilter;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+		filterFilterRegistrationBean.setFilter(new LogFilter());
+		filterFilterRegistrationBean.setOrder(1);
+		filterFilterRegistrationBean.addUrlPatterns("/*");
+		filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+		return filterFilterRegistrationBean;
+	}
+}
