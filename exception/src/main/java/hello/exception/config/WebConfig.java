a8e8138bb16642ef57f0b340dc20b363cd948909
장우진 <dnwls16071@naver.com>
2024-08-18 23:09:25 +0900

    :sparkles: 서블릿 예외 처리(인터셉터)

--- exception/src/main/java/hello/exception/config/WebConfig.java
+++ exception/src/main/java/hello/exception/config/WebConfig.java
@@ -1,17 +1,19 @@
 package hello.exception.config;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Bean
+	//@Bean
 	public FilterRegistrationBean filterRegistrationBean() {
 		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
 		filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -19,5 +21,13 @@ public class WebConfig implements WebMvcConfigurer {
 		filterFilterRegistrationBean.addUrlPatterns("/*");
 		filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
 		return filterFilterRegistrationBean;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+				.order(1)
+				.addPathPatterns("/*")
+				.excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
 	}
 }
