498ae0ccf0a34407b7ff6963461c992f71254513
장우진 <dnwls16071@naver.com>
2024-08-19 13:27:58 +0900

    :sparkles: 스프링 부트 제공 오류 처리

---
+++ apiexception/src/main/java/hello/apiexception/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package hello.apiexception.config;
+
+import hello.apiexception.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+				.order(1)
+				.addPathPatterns("/*")
+				.excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
+	}
+}
---
+++ apiexception/src/main/java/hello/apiexception/config/WebServerCustomizer.java
@@ -0,0 +1,19 @@
+package hello.apiexception.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+// 스프링 부트에서 제공하는 tomcat의 오류 페이지 대신 사용할 오류 페이지 커스터마이징
+//@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+	@Override
+	public void customize(ConfigurableWebServerFactory factory) {
+		ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+		ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+		ErrorPage errorPage = new ErrorPage(RuntimeException.class, "/error-page/500");
+		factory.addErrorPages(errorPage, errorPage404, errorPage500);
+	}
+}
