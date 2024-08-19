5b5b6a36ca32d79480f10fcb0629633f4a230b91
장우진 <dnwls16071@naver.com>
2024-08-19 16:20:57 +0900

    :sparkles: API 예외 처리 (1)

--- apiexception/src/main/java/hello/apiexception/api/ApiExceptionController.java
+++ apiexception/src/main/java/hello/apiexception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.apiexception.api;
 
+import hello.apiexception.exception.BadRequestException;
 import hello.apiexception.exception.UserException;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,10 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -56,6 +54,16 @@ public class ApiExceptionController {
 
 		Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+	}
+
+	@RequestMapping(value = "/api/response-status-ex1")
+	public String responseStatus() {
+		throw new BadRequestException();
+	}
+
+	@GetMapping("/api/default-handler-ex")
+	public String defaultHandler(@RequestParam(name = "data") Integer data) {
+		return "ok";
 	}
 
 	@Data
